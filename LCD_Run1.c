a816528c302a3c2676dd3227ce191374f1b84343
humbleSeraph <OsagieIgbeare@dn2lk5m42.stanford.edu>
2014-08-18 14:34:37 -0700

    SPI Debugging
    commented out transmitting in the interrupt

--- LCD_Run1.c
+++ LCD_Run1.c
@@ -12,25 +12,53 @@ author: Osagie Igbeare
 /**************** Header Files *********************/
 
 #include "BitDefs.h"
-#include <htc.h>
+//#include <htc.h>
+#include <xc.h>
 #include "pic.h"
 #include "chip_select.h"
-//#include "pic16lf1827.h"
+
 
 
 /***************** Configuration Macros ***************/
 
-__CONFIG(FCMEN_OFF & IESO_OFF & FOSC_XT & WDTE_OFF & MCLRE_ON & PWRTE_OFF & BOREN_OFF
-		& LVP_ON & WRT_OFF & CPD_OFF & CP_OFF);
+//__CONFIG(FCMEN_OFF & IESO_OFF & FOSC_LP & WDTE_OFF & MCLRE_ON & PWRTE_OFF & BOREN_OFF
+//		& LVP_ON & WRT_OFF & CPD_OFF & CP_OFF);
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+// CONFIG1
+#pragma config FOSC = LP        // Oscillator Selection (LP Oscillator, Low-power crystal connected between OSC1 and OSC2 pins)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
+#pragma config CLKOUTEN = ON    // Clock Out Enable (CLKOUT function is enabled on the CLKOUT pin)
+#pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PLLEN = OFF      // PLL Enable (4x PLL enabled)
+#pragma config STVREN = ON     // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
 
 
 /***************** # Defines *****************/
 #define lcd_data BIT3HI
 #define lcd_command BIT3LO
+#define hangTime 1000
 
 /*************** module level variables ************/
 
 static char counter; 
+int x;
+int i = 0;
+char dummy;
+static char LCD_Init[6]; 
 
 
 
@@ -41,6 +69,8 @@ void InitTimers(void);
 void InitInterrupts(void);
 void InitComm(void);
 void NokiaInit(void);
+void Delay(int waitTime); 
+void SightPin_B0(void);
 
 
 
@@ -53,103 +83,195 @@ void InitPorts()
 	ANSELB = 0x00;			// Port B pins are digital
 
 	TRISA = 0b11111010;		// 1 - input, 0 - output, RA2, RA0 are outputs
-	TRISB = 0b10000011;		// 1 - input, 0 - output, RB2 - RB6 is an output
+	TRISB = 0b10000010;		// 1 - input, 0 - output, RB0, RB2 - RB6 is an output
 
 	PORTA = 0b11111111;			// initialize LED to OFF
 	PORTB = 0b11111111;
 
+	
+	//OSCCON = 0x68;
+	APFCON0 = 0x00;
+	
 }
 
 void InitTimers()
 {
 
 	T2CON = 0b01111110;		// Fosc / (4 instruct * 16 prescale * 16 postscale * 60 PR2) = 65 Hz
-	PR2 = 250;
+	PR2 = 1;
 }
 
 void InitInterrupts()
 {
 
-	PIE1 = 0b00001010; 		// Enable TMR2IE, interrupt when Timer 2 matches PR2
+	PIE1 = 0b00000010; 		// Enable TMR2IE, interrupt when Timer 2 matches PR2
+							// Enable SSP1IE, interrupt for MSSP1 (aka SPI1)
 							// Bit 3 high - enable MSSP interrupt 	
 	INTCON = 0b11000000;	// Enable GIE, Enable PEIE
 	
 }
 
+
 void InitComm()
 {
 	// setup SPI-1 (aka SSP) to communicate with Nokia LCD screen
 
-	SSP1ADD = 0;			// Baud Rate = Fosc / ((SSP1ADD + 1)(4))
+	SSP1ADD = 0;                                    // Baud Rate = Fosc / ((SSP1ADD + 1)(4))
 							// since Fosc = 4 MHz, Baud Rate = 1 MHz
+							// since SSP1ADD = 0 is not supported same timing is achieved
+							// by setting bits <3:0> of SSPM all to 0's (see below)
 
-	SSPSTAT = 0;			// data on rising edge, data @ middle
-	WCOL = 0; 	 			// no collision
-	SSPOV = 0; 				// no overflow
-	SSP1EN = 1; 			// SSP Enable
+	SSP1STATbits.SMP = 0;				// data on rising edge, data @ middle
+	SSP1CON1bits.WCOL = 0; 	 			// no collision
+	SSP1CON1bits.SSPOV = 0; 			// no overflow
+	
 
-	CKP = 1; 				// idle high 
-	CKE = 1;				// sample even edges 
+	SSP1CON1bits.CKP = 1; 				// idle high 
+	SSP1STATbits.CKE = 1;				// sample even edges 
 
-	SSP1M3 =  0;			// Set LF1827 as Master, clock rate Fosc / 4
-	SSP1M2 = 0;
-	SSP1M1 = 0; 
-	SSP1M0 = 0; 
+	SSP1CON1bits.SSPM3 =  0;			// Set LF1827 as Master, clock rate Fosc / 4
+	SSP1CON1bits.SSPM2 = 0;
+	SSP1CON1bits.SSPM1 = 0; 
+	SSP1CON1bits.SSPM0 = 0;
 
-
+        SSP1CON1bits.SSPEN = 1; 			// SSP Enable
 
 }
 
 void NokiaInit()
 {
+
+	i = 0; 
+	// LCD_Init array populated with initialization sequence
+
+	LCD_Init[0] = 0x21;			// tell LCD extended commands to 
+	LCD_Init[1] = 0xB0;			// set LCD Vop (contrast) ** parameter to mess with if screen doesn't display **** 
+	LCD_Init[2] = 0x04;			// set temp coefficient
+	LCD_Init[3] = 0x13;			// LCD Bias mode 1:48 (if not working, try 0x13)
+	LCD_Init[4] = 0x20;			// back to regular commands
+	LCD_Init[5] = 0x0C;			// enable normal display (dark on light), horiz addressing
+
+
 	// initialization sequence for the PCD8544 driver on the Nokia LCD
 	// beginning with RESET 
-
+	
+	/*
 	PORTA &= BIT0LO;
-	_delay(1000);
+	Delay(hangTime); 
 	PORTA |= BIT0HI;
+	*/
+	
+	PORTB &= lcd_command;           // tell LCD commands are coming
 
-	PORTB &= lcd_command;	// tell LCD commands are coming 
+	
+	
+	PORTB &= BIT5LO;			// lower SCE line to begin transmission
+	
+	//SightPin_B0();
 
-	SSPBUF = 0x21;			// tell LCD extended commands to follow
-	SSPBUF = 0xE0;			// set LCD Vop (contrast) ** parameter to mess with if screen doesn't display **** 
-	SSPBUF = 0x04;			// set temp coefficient
-	SSPBUF = 0x14; 			// LCD Bias mode 1:48 (if not working, try 0x13)
+        PIR1bits.SSP1IF = 0;
 
-	SSPBUF = 0x20;			// back to regular commands
-	SSPBUF = 0x0C; 			// enable normal display (dark on light), horiz addressing
+        SSP1CON1bits.WCOL = 0; 
 
+	SSP1BUF =  0xC3;
+	
+
+        if(SSP1CON1bits.WCOL == 0)
+	{
+		SSP1BUF = 0xC3;
+	}
+	
+
+	
+
+
+	/* 
+	SSP1BUF = 0x21;			// tell LCD extended commands to 
+	SSP1BUF = 0xB0;			
+	SSP1BUF = 0x04;			
+	SSP1BUF = 0x13; 			
+
+	SSP1BUF = 0x20;			
+	SSP1BUF = 0x0C; 
+	*/			
+
+	PORTB |= BIT5HI;		// raising SCE line at the end of transmission0
 }
 
 void interrupt ISR()
 {
-
 	counter++; 
 	if (TMR2IF)
 	{
 		if ((counter % 2) != 0)
 		{
 			PORTA |= BIT2HI;
-			PORTB &= BIT6LO;
+			//PORTB &= BIT6LO;
 			
 		}
 		else 
 		{
 			PORTA &= BIT2LO;
-			PORTB |= BIT6HI;
+			//PORTB |= BIT6HI;
 			counter = 0; 
 		}
 
 		TMR2IF = 0;		// clears the TIMR2IF (timer 2 interrupt flag)
 
 	}
+	/*
+	if (SSP1IF)
+	{	
+				
+		if (i < 6)
+		{
+			SSP1BUF = LCD_Init[i];
+			i++;
+			SightPin_B0();
+		}
+		
+		SSP1BUF = LCD_Init[i];
+
+		SSP1IF = 0;	
+
+	}
+	*/
+	
 	return; 
 
 }
 
+void Delay(int waitTime)
+{
+	x = 0;
+	while(x < waitTime)
+	{
+		x += 1;
+	}
+}
+
+/***********************************************************/
+/******************** Debugging Library ********************/
+
+void SightPin_B0(void)
+{
+	// toggles pin B0 for debugging purposes
+	if ((PORTB & BIT0HI) == BIT0HI)
+	{
+		PORTB &= BIT0LO;
+
+	}
+	else
+	{
+		PORTB |= BIT0HI; 
+	}
+
+}
 
 
+/********************************************************/
 /******** Main - which actually runs the code ***********/
+/********************************************************/
 
 void main ()
 {
@@ -159,10 +281,10 @@ void main ()
 	InitTimers();
 	InitInterrupts();
 	InitComm(); 
-	NokiaInit();
+	//NokiaInit();
 	while(1)
 	{
-
+		NokiaInit();
 	}
 
 
